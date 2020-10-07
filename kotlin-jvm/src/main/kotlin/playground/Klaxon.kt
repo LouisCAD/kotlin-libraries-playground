1ce2b13ea2e9656ba475917552a97660c5472bc1
oppongjr <oppong751@gmail.com>
2020-10-07 15:03:35 +0000

    Add sample for Klaxon. Closes #43

---
+++ kotlin-jvm/src/main/kotlin/playground/Klaxon.kt
@@ -0,0 +1,22 @@
+@file:Suppress("PackageDirectoryMismatch")
+package playground.klaxon
+
+import com.beust.klaxon.Klaxon
+import playground.shouldBe
+
+
+/**
+ * klaxon - A library to parse JSON in Kotlin.
+ *
+ * -  [klaxon: GitHub](https://github.com/cbeust/klaxon)
+ */
+fun main() {
+    println("# klaxon - A library to parse JSON in Kotlin.")
+    val json = """{"id" : 1, "name" : "Adam"}"""
+    val user = User(id = 1, name = "Adam")
+
+    Klaxon().parse<User>(json) shouldBe user
+    Klaxon().toJsonString(user) shouldBe json
+}
+
+internal data class User(val id: Int, val name: String)
