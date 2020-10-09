7becc6727ea667c028629bf518e5498a81b00f96
Katerine Salazar Gonzalez <katogarabato1@gmail.com>
2020-10-09 11:07:32 +0200

    Implement Picnic

--- kotlin-jvm/src/main/kotlin/playground/Picnic.kt
+++ kotlin-jvm/src/main/kotlin/playground/Picnic.kt
@@ -2,6 +2,9 @@
 
 package playground.picnic
 
+import com.jakewharton.picnic.table
+import playground.shouldBe
+
 /**
  * JakeWharton/picnic : A Kotlin DSL and Java/Kotlin builder API for constructing HTML-like tables which can be rendered to text.
  *
@@ -10,6 +13,44 @@ package playground.picnic
 fun main() {
     println()
     println("# JakeWharton/picnic : A Kotlin DSL and Java/Kotlin builder API for constructing HTML-like tables which can be rendered to text.")
+    println("Who? Guests? Brings")
 
+
+    val table = table {
+        cellStyle {
+            border = true
+            paddingLeft = 1
+            paddingRight = 1
+        }
+        header {
+            row("Who?", "Guests?", "Brings what?")
+        }
+        guests.forEach { guest ->
+            row(guest.who, guest.guests.joinToString(), guest.brings.joinToString())
+        }
+    }
+    "\n$table" shouldBe  """
+┌─────────┬──────────────────┬─────────────────┐
+│ Who?    │ Guests?          │ Brings what?    │
+├─────────┼──────────────────┼─────────────────┤
+│ Alice   │ Anita            │ Quiche          │
+├─────────┼──────────────────┼─────────────────┤
+│ Bob     │                  │ Beer            │
+├─────────┼──────────────────┼─────────────────┤
+│ Charlie │ Celia, Christina │ Guitar, Peanuts │
+└─────────┴──────────────────┴─────────────────┘"""
 }
+
+
+data class PicnicGuest(
+    val who: String,
+    val guests: List<String>,
+    val brings: List<String>
+)
+
+val guests = listOf(
+    PicnicGuest(who = "Alice", guests = listOf("Anita"), brings = listOf("Quiche")),
+    PicnicGuest(who = "Bob", guests = emptyList(), brings = listOf("Beer")),
+    PicnicGuest(who = "Charlie", guests = listOf("Celia", "Christina"), brings = listOf("Guitar", "Peanuts")),
+)
 
