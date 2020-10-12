2056322970b61715a00bad022b51f4966b998072
Oscar Sequeiros <oscar.sequeiros_ex@scotibank.cl>
2020-10-12 00:09:11 -0300

    Calling machinestate.main from global main.

--- kotlin-jvm/src/main/kotlin/playground/_main.kt
+++ kotlin-jvm/src/main/kotlin/playground/_main.kt
@@ -27,6 +27,7 @@ fun main() {
     playground.picnic.main()
     playground.retrofit.main()
     playground.sqldelight.main()
+    playground.statemachine.main()
     /**
      * Keep the list sorted to minimize merge conflicts on pull-requests!
      */
