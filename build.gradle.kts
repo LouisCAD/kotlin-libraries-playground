4e0e262558895132d28caf9c5e92370182580250
Jean-Michel Fayard <jmfayard@gmail.com>
2020-10-15 12:08:44 +0200

    ✚ Gradle Versions Plugin (#69)
    ✚ Gradle Versions Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     kotlin("jvm").apply(false)
+    id("com.github.ben-manes.versions")
 }
 
 
@@ -36,4 +38,26 @@ tasks.register("runOnGitHub") {
 tasks.register<DefaultTask>("hello") {
     group = "Custom"
     description = "Minimal task that do nothing. Useful to debug a failing build"
+}
+
+
+
+/**
+ * The Gradle Versions Plugin is another Gradle plugin to discover dependency updates
+ * plugins.id("com.github.ben-manes.versions")
+ * Run it with $ ./gradlew --scan dependencyUpdates
+ * https://github.com/ben-manes/gradle-versions-plugin
+ * **/
+tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+    checkConstraints = true
 }
