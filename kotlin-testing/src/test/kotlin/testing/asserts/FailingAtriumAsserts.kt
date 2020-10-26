d58c485f3e4f944975e813079cd3a56ab473f4a3
oppongjr <oppong751@gmail.com>
2020-10-26 09:08:47 +0000

    Moved atrium tests into separate class.
    Added assertions for map and lists

--- kotlin-testing/src/test/kotlin/testing/asserts/FailingAtriumAsserts.kt
+++ kotlin-testing/src/test/kotlin/testing/asserts/FailingAtriumAsserts.kt
@@ -1,18 +1,34 @@
 package testing.asserts
 
-import ch.tutteli.atrium.api.fluent.en_GB.isEmpty
+
+import ch.tutteli.atrium.api.fluent.en_GB.*
 import ch.tutteli.atrium.api.verbs.expect
 import org.junit.jupiter.api.Test
-import testing.common.UseCase
 
 class FailingAtriumAsserts {
-
     @Test
     fun `failing assertion group`() {
         expect(17) {
             isLessThan(4)
             isGreaterThan(20)
             isEqualComparingTo(17)
+        }
+    }
+
+    @Test
+    fun `number is greater than 20 and less than 19`() {
+        expect(8 + 10).isGreaterThan(20).isLessThan(19)
+    }
+
+    @Test
+    fun `list should contain values greater than 10`() {
+        expect(listOf(2, 5, 6, 7, 8,)).all { isGreaterThan(10) }
+    }
+
+    @Test
+    fun `all keys should start with b`() {
+        expect(mapOf("benny" to 1, "curry" to 2, "derry" to 3)){
+            keys { all { startsWith("b") } }
         }
     }
 }
