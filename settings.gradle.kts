2bb2d9c22ee194a13fcdf20c756d7b6bbea6c536
Jean-Michel Fayard <jmfayard@gmail.com>
2020-11-15 09:26:26 +0100

    To be merged once refreshVersions 0.9.8 is released
    See https://github.com/jmfayard/refreshVersions/pull/280
    See https://github.com/jmfayard/refreshVersions/issues/290

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,34 @@
 import de.fayard.refreshVersions.bootstrapRefreshVersions
 
-buildscript {
-    repositories { mavenLocal(); gradlePluginPortal() }
-    dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
+pluginManagement {
+    val (stableVersion, devVersion, githubPR) = listOf("0.9.8", null, null)
+    fun module(name: String) = when {
+        githubPR != null -> "com.github.jmfayard:$name:PR${githubPR}-SNAPSHOT"
+        devVersion != null -> "de.fayard.refreshVersions:$name:$devVersion"
+        else -> "de.fayard.refreshVersions:$name:$stableVersion"
+    }
+
+    repositories {
+        gradlePluginPortal()
+        if (devVersion != null) mavenLocal()
+        if (githubPR != null) maven("https://jitpack.io")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "de.fayard.refreshVersions" -> useModule(module("refreshVersions"))
+                "de.fayard.buildSrcLibs" -> useModule(module(
+                    if (githubPR != null) "refreshVersions" else "buildSrcLibs"
+                ))
+            }
+        }
+    }
 }
 
 plugins {
     id("com.gradle.enterprise") version "3.4.1"
+    id("de.fayard.buildSrcLibs")
+    id("de.fayard.refreshVersions")
 }
 
 // https://dev.to/jmfayard/the-one-gradle-trick-that-supersedes-all-the-others-5bpg
