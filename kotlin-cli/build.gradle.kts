6464cc4af68b21c4696193c9a86490ee262ce632
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-03 12:13:23 +0200

    kotlin-cli: make git-standup multiplatform
    Co-authored-by: Louis CAD <louis.cognault@gmail.com>

--- kotlin-cli/build.gradle.kts
+++ kotlin-cli/build.gradle.kts
@@ -17,14 +17,29 @@ dependencies {
 
 
 kotlin {
-    // TODO how to support windows and Linux?
-    macosX64("native") {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
         binaries {
-            executable()
+            executable {
+                entryPoint = "main"
+            }
         }
     }
     sourceSets {
-        val nativeMain by getting {
+        getByName("nativeTest") {
+        // TODO: add tests
+        // TODO: add GitHub actions
+
+        }
+        getByName("nativeMain") {
             dependencies {
                 implementation("com.github.ajalt.clikt:clikt:_")
             }
@@ -40,10 +55,17 @@ tasks.register<Copy>("install") {
     val destDir = "/usr/local/bin"
     dependsOn("runDebugExecutableNative")
     from("build/bin/native/debugExecutable") {
-        rename { "standup" }
+        rename { "git-standup" }
     }
     into(destDir)
     doLast {
         println("$ standup installed into $destDir")
     }
+}
+
+
+// TODO: copy to /usr/local/share/bash-completion/completions
+tasks.register<Exec>("dq") {
+    val completeCommand = "standup --generate-completion zsh > completion-standup.zsh"
+    commandLine = completeCommand.split(" ")
 }
