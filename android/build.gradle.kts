2880a85687e0f1889af4c4aad390f102b4aca70c
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-24 20:54:20 +0200

    Convert to dependency notations

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -35,13 +35,13 @@ android {
 
 dependencies {
 
-    implementation(kotlin("stdlib", "_"))
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("com.google.android.material:material:1.3.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
-    testImplementation("junit:junit:4.+")
-    implementation("app.cash.contour:contour:1.1.0")
+    implementation(Kotlin.stdlib)
+    implementation(AndroidX.core.ktx)
+    implementation(AndroidX.appCompat)
+    implementation(AndroidX.constraintLayout)
+    implementation(Google.android.material)
+    androidTestImplementation(AndroidX.test.ext.junit)
+    androidTestImplementation(AndroidX.test.espresso.core)
+    testImplementation(Testing.junit4)
+    implementation("app.cash.contour:contour:_")
 }
